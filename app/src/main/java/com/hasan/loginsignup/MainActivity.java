1a22a7b49735c146e927fc04257a512ebd372981
Md Hasanuzzaman <dr.has82@gmail.com>
2024-01-07 03:47:17 +0600

    Change Primary color

--- app/src/main/java/com/hasan/loginsignup/MainActivity.java
+++ app/src/main/java/com/hasan/loginsignup/MainActivity.java
@@ -1,8 +1,13 @@
 package com.hasan.loginsignup;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.hasan.loginsignup.auth.LoginActivity;
+import com.hasan.loginsignup.auth.SignupActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signupButton, loginButton;
+
+        signupButton = findViewById(R.id.signupButton);
+        loginButton = findViewById(R.id.loginButton);
+
+        signupButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
+
+        loginButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
